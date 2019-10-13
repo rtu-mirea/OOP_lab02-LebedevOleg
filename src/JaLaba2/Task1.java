27f762fdf00a524d057c255aae1e92990a318bb5
LebedevOleg <gnus168@yandex.ru>
2019-10-13 21:58:02 +0300

    доделан ввод с проверкой

--- src/JaLaba2/Task1.java
+++ src/JaLaba2/Task1.java
@@ -10,17 +10,25 @@ public class Task1 {
         text = "";
     }
     public void setText() {
+        boolean OK = true;
         System.out.println("Введите текс из чисел(числа разделяются ТОЛЬКО точкой).После числа через запятую пишется система счисления");
-        text = in.nextLine();
+        while (OK) {
+            text = in.nextLine();
+            OK = false;
+            for (int i = 0; i < text.length(); i++) {
+                if ((text.charAt(i) >= 48 && text.charAt(i) <= 57) || text.charAt(i) == 46 || text.charAt(i) == 44);
+                else {
+                    System.out.println("Вы не правильно ввели текст. Пожалуйста введите число ',' система счисления '.' следующее число если нужно");
+                    OK = true;
+                    break;
+                }
+            }
+        }
     }
     public String getText() {
         return text;
     }
     public void changeText(){
-        for(int i = 0;i<text.length();i++)
-        {
-            if(text[i]>= 48)
-        }
         String x[] = text.split("\\.");
         temp1 =new String[x.length][2];
         for(int i= 0; i<x.length;i++){
@@ -31,8 +39,12 @@ public class Task1 {
            }
            temp1[i][0] = z[0];
            temp1[i][1] = z[1];
+        }
+        int summ = 0;
+        int t1 = 0, t2 = 0;
+        for(int i = 0; i<temp1.length;i++){
+            t2 = Integer.parseInt(temp1[i]);//whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 
         }
-
     }
 }
